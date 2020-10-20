4b2c25d7734939c86895cd7cbb17787fb85974ef
nimsajj <nimsaj.dev@gmail.com>
2020-10-20 12:38:05 +0200

    feature: create, update and get entitity and update views

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,20 +18,54 @@ class BlogController extends AbstractController
      */
     public function index()
     {
-        return $this->render('blog/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $articles = $em->getRepository(Article::class)->findBy(
+            ["isPublished" => true],
+            ['publishedAt' => 'desc']
+        );
+
+        return $this->render('blog/index.html.twig', ["articles" => $articles]);
     }
 
     /**
      * @Route("/add", name="article_add")
      */
-    public function add(Request $request)
+    public function add(Request $request, SluggerInterface $slugger)
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            return new Response('Le formulaire a été soumis...');
+
+            if ($article->getIsPublished()) {
+                $article->setPublishedAt(new \DateTime());
+            }
+
+            if ($article->getPicture() !== null) {
+                $file = $form->get('picture')->getData();
+                $filemanme = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($filemanme);
+                $fileName =  $safeFilename . '.' . uniqid() . '.' . $file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('upload_directory'),
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $article->setPicture($fileName);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return new Response('Le formulaire a été enregistré');
         }
 
         return $this->render('blog/add.html.twig', [
@@ -38,22 +74,62 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/show/{slug}", name="article_show")
+     * @Route("/show/{id}", name="article_show")
      */
-    public function show(string $slug)
+    public function show(Article $article)
     {
         return $this->render('blog/show.html.twig', [
-            'slug' => $slug,
+            'article' => $article,
         ]);
     }
 
     /**
      * @Route("/edit/{id}", name="article_edit", requirements={"id"="\d+"})
      */
-    public function edit(int $id)
+    public function edit(Article $article, Request $request, SluggerInterface $slugger)
     {
+        $oldPicture = $article->getPicture();
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setUpdatedAt(new \DateTime());
+
+            if ($article->getIsPublished()) {
+                $article->setPublishedAt(new \DateTime());
+            }
+
+            if ($article->getPicture() !== null && $article->getPicture() !== $oldPicture) {
+                $file = $form->get('picture')->getData();
+                $filemanme = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($filemanme);
+                $fileName =  $safeFilename . '.' . uniqid() . '.' . $file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('upload_directory'),
+                        $fileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $article->setPicture($fileName);
+            } else {
+                $article->setPicture($oldPicture);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return new Response('L\'article a bien été modifier.');
+        }
+
         return $this->render('blog/edit.html.twig', [
-            'id' => $id,
+            'article' => $article,
+            'form' => $form->createView()
         ]);
     }
 
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use App\Repository\ArticleRepository;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -46,7 +47,7 @@ class Article
     private $publishedAt;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      */
     private $updatedAt;
 
